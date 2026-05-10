ac78d78d79feaf25c41c2a7adf06f67eb32f7222
GregTish <tishchuk167@gmail.com>
2026-05-10 23:42:35 +0300

    done

--- assignment1/tailwind.config.js
+++ assignment1/tailwind.config.js
@@ -1,17 +1,3 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#007bff',
-        secondary: '#6c757d',
-        danger: '#dc3545',
-        success: '#28a745',
-      },
-    },
-  },
-  plugins: [],
-}
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+};
